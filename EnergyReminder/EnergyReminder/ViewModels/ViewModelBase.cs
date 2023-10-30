78188be4d747076fd68b6776d541d4df6db46aa8
York <zhuyanfang@outlook.com>
2023-10-30 20:10:13 +0800

    Initial Commit

---
+++ EnergyReminder/EnergyReminder/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace EnergyReminder.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ EnergyReminder/EnergyReminder/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace EnergyReminder.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
